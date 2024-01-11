cb82512e10b9b230a3e81ee85c1937521a393d89
nescpro <salazarnestor1996@gmail.com>
2024-01-11 12:02:30 -0500

    Create the routes with react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import About from './routes/about';
+import Profile from './routes/profile';
+import SignIn from './routes/sign-in';
+import SignUp from './routes/sign-up';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: 'sign-in',
+				element: <SignIn />,
+			},
+			{
+				path: 'sign-out',
+				element: <SignUp />,
+			},
+			{
+				path: 'about',
+				element: <About />,
+			},
+			{
+				path: 'profile',
+				element: <Profile />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<RouterProvider router={router} />,
+);
